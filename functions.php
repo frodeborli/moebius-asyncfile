562fe8a82a0917aa3fb88f54f6223859358a4364
Frode Børli <frode@ennerd.com>
2022-05-31 08:05:55 +0000

    Updated stability requirements

--- functions.php
+++ functions.php
@@ -1,11 +1,6 @@
 <?php
 // Make sure we don't enable it too early, need loop and coroutine to be ready
 
-class_exists(Moebius\Loop\Readable::class);
-class_exists(Moebius\Loop\Writable::class);
-class_exists(Moebius\Loop\Timer::class);
-class_exists(Moebius\Promise::class);
-Moebius\Loop::await(new Moebius\Loop\Timer(0));
 Moebius\Coroutine::go(function() {
     Moebius\AsyncFile\FileStreamWrapper::register();
 });
