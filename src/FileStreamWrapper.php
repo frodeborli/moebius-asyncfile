a18857ca26fdfd0ea7b3d03b14edbf119cc7896d
Frode Børli <frode@ennerd.com>
2022-06-01 08:00:36 +0000

    File stream wrapper rewritten to not rely on Unblocker

--- src/FileStreamWrapper.php
+++ src/FileStreamWrapper.php
@@ -1,159 +1,297 @@
 <?php
 namespace Moebius\AsyncFile;
 
+use Fiber;
+use Closure;
 use Moebius\Coroutine as Co;
 use Moebius\Coroutine\Unblocker;
+use Moebius\Loop;
 
-class FileStreamWrapper extends Unblocker {
+class_exists(Unblocker::class) or die("not found");
+class_exists(\Moebius\Loop\Readable::class);
 
-    const PROTOCOL = 'file';
+use const STREAM_REPORT_ERRORS;
 
-    protected static bool $registered = false;
-    protected static ?int $lastErrorCode = null;
-    protected static ?string $lastErrorMessage = null;
+class FileStreamWrapper {
+
+    private static bool $registered = false;
+    private static bool $ready = false;
 
     public static function register(): void {
-        if (self::$registered) {
-            throw new \Exception("Already registered");
+        if (!self::$ready) {
+            $fp = \fopen(__FILE__, 'r');
+            $fp2 = Unblocker::unblock($fp);
+            \stream_get_contents($fp2);
+            self::$ready = true;
         }
-        stream_wrapper_unregister(static::PROTOCOL);
-        stream_wrapper_register(static::PROTOCOL, self::class);
+        if (self::$registered) {
+            throw new \LogicException("Already registered");
+        }
+        self::$registered = true;
+        \stream_wrapper_unregister('file');
+        \stream_wrapper_register('file', self::class);
     }
 
     public static function unregister(): void {
         if (!self::$registered) {
-            throw new \Exception("Not registered");
+            throw new \LogicException("Not registered");
         }
-        stream_wrapper_unregister(static::PROTOCOL);
-        stream_wrapper_restore(static::PROTOCOL);
+        self::$registered = false;
+        \stream_wrapper_unregister('file');
+        \stream_wrapper_restore('file');
     }
 
-    protected static function wrap(bool $unregister, callable $callback, mixed ...$args): mixed {
-        if ($unregister) {
-            stream_wrapper_unregister(static::PROTOCOL);
-            stream_wrapper_restore(static::PROTOCOL);
+    private static function wrap(Closure $callable) {
+        self::unregister();
+        try {
+            return $callable();
+        } catch (\Throwable $e) {
+            echo get_class($e).": ".$e->getMessage()."\n".$e->getTraceAsString()."\n";
+            throw $e;
+        } finally {
+            self::register();
         }
-        self::$lastErrorCode = null;
-        self::$lastErrorMessage = null;
-        set_error_handler(function(int $code, string $message, string $file, int $line) use (&$errorCode, &$errorMessage) {
-            self::$lastErrorCode = $errorCode;
-            self::$lastErrorMessage = $errorMessage;
+    }
+
+    private bool $blocking;
+    private ?float $readTimeout = null;
+    private ?float $writeTimeout = null;
+    private $fp; // stream
+    private $dh; // dirhandle
+
+    public function stream_cast(int $cast_as) {
+        return $this->fp;
+    }
+
+    /**
+     * open the stream
+     *
+     * @param string      $path        the path to open
+     * @param string      $mode        mode for opening
+     * @param int         $options     options for opening
+     * @param string|null $opened_path full path that was actually opened
+     */
+    public function stream_open(string $path, string $mode, int $options, ?string $opened_path = null): bool
+    {
+        //echo "stream_open($path, $mode)\n";
+        $this->fp = self::wrap(function() use ($path, $mode, $options) {
+            if (strpos($mode, 'n')===false) {
+                $this->blocking = true;
+                $mode .= 'n';
+            } else {
+                $this->blocking = false;
+            }
+            return \fopen($path, $mode, 0 === $options & \STREAM_USE_PATH, $this->context);
         });
-        $t = hrtime(true);
-        $result = $callback(...$args);
-        $t = hrtime(true) - $t;
-        if ($t > 10000000) { // 1/100th of a second is considered blocking
-            // This call appears to be blocking
-            $bt = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2);
-            array_shift($bt);
-            error_log("Moebius/FileStreamWrapper::".$bt[0]['function']." took ".($t/1000000)." ms and appears to block", 0);
-        }
-        restore_error_handler();
-        if ($unregister) {
-            stream_wrapper_unregister(static::PROTOCOL);
-            stream_wrapper_register(static::PROTOCOL, self::class);
-        }
-        return $result;
+
+        return !!$this->fp;
     }
 
-    protected $dirHandle = null;
+    public function stream_close(): void {
+        \fclose($this->fp);
+    }
+
+    public function stream_eof(): bool {
+        return \feof($this->fp);
+    }
+
+    public function stream_flush(): bool {
+        $this->writable();
+        return \fflush($this->fp);
+    }
+
+    public function stream_flock(int $operation): bool {
+        return flock($this->fp, $operation, $would_block);
+    }
+
+    public function stream_metadata(string $path, int $option, $value): bool {
+        return self::wrap(function() use ($path, $options, $value) {
+            switch ($options) {
+                case STREAM_META_TOUCH:
+                    if (!empty($value)) {
+                        $success = \touch($path, $value[0], $value[1]);
+                    } else {
+                        $success = \touch($path);
+                    }
+                    break;
+                case STREAM_META_OWNER_NAME:
+                    // fall through
+                case STREAM_META_OWNER:
+                    $success = \chown($path, $value);
+                    break;
+                case STREAM_META_GROUP_NAME:
+                    // fall through
+                case STREAM_META_GROUP:
+                    $success = \chgrp($path, $value);
+                    break;
+                case STREAM_META_ACCESS:
+                    $success = \chmod($path, $value);
+                    break;
+                default:
+                    $success = false;
+            }
+            return $success;
+        });
+    }
+
+    public function stream_read(int $count): string|false {
+        if ($this->blocking) {
+            do {
+                $this->readable();
+                $chunk = \fread($this->fp, $count);
+            } while ($chunk === '');
+            return $chunk;
+        } else {
+            $this->readable();
+            return \fread($this->fp, $count);
+        }
+    }
+
+    public function stream_seek(int $offset, int $whence=SEEK_SET): bool {
+        return 0 === \fseek($this->fp, $offset, $whence);
+    }
+
+    public function stream_set_option(int $option, int $arg1, int $arg2): bool {
+        switch ($option) {
+            case STREAM_OPTION_BLOCKING:
+                // The method was called in response to stream_set_blocking()
+                $this->blocking = $arg1 !== 0;
+                return true;
+
+            case STREAM_OPTION_READ_TIMEOUT:
+                // The method was called in response to stream_set_timeout()
+                $this->readTimeout = $arg1 + ($arg2 / 1000000);
+                \stream_set_timeout($this->fp, $arg1, $arg2);
+                return true;
+
+            case STREAM_OPTION_WRITE_BUFFER:
+                // The method was called in response to stream_set_write_buffer()
+                return 0 === \stream_set_write_buffer($this->fp, $arg2);
+
+            case STREAM_OPTION_READ_BUFFER:
+                return 0 === \stream_set_read_buffer($this->fp, $arg2);
+
+            default :
+                trigger_error("Unknown stream option $option with arguments ".json_encode($arg1)." and ".json_encode($arg2), E_USER_ERROR);
+                return false;
+        }
+        return false;
+    }
+
+    public function stream_stat(): array|false {
+        return \fstat($this->fp);
+    }
+
+    public function stream_tell(): int {
+        return \ftell($this->fp);
+    }
+
+    public function stream_truncate(int $new_size): bool {
+$this->writable();
+        return \ftruncate($this->fp, $new_size);
+    }
+
+    public function stream_write(string $data): int {
+$this->writable();
+        return \fwrite($this->fp, $data);
+    }
 
     public function dir_closedir(): bool {
-//        $this->suspend();
-        self::wrap(false, closedir(...), $this->dirHandle);
-        return true;
+        \closedir($this->dh);
     }
 
     public function dir_opendir(string $path, int $options=0): bool {
-//        $this->suspend();
-        return !!($this->dirHandle = @self::wrap(true, opendir(...), $path, $this->context));
+        return static::wrap(function() use ($path, $options) {
+            $this->dh = \opendir($path, $options);
+            return !!$this->dh;
+        });
     }
 
     public function dir_readdir(): string|false {
-        return self::wrap(false, readdir(...), $this->dirHandle);
+        return \readdir($this->dh);
     }
 
     public function dir_rewinddir(): bool {
-//        $this->suspend();
-        return self::wrap(false, rewinddir(...), $this->dirHandle);
-    }
-
-    public function mkdir(string $path, $mode, int $options=0): bool {
-//        $this->suspend();
-        return self::wrap(true, mkdir(...), $path, $mode, (bool) ($options & STREAM_MKDIR_RECURSIVE));
-    }
-
-    public function rename(string $pathFrom, $pathTo): bool {
-//        $this->suspend();
-        return self::wrap(true, rename(...), $pathFrom, $pathTo);
-    }
-
-    public function rmdir(string $path): bool {
-//        $this->suspend();
-        return self::wrap(true, rmdir(...), $path);
-    }
-
-    public function stream_open(string $path, string $mode, int $options, ?string &$opened_path): bool {
-        /**
-         * Modify the mode so that we can open this file in a non-blocking manner
-         */
-        $isNonBlocking = strpos($mode, 'n') !== false;
-        $fp = self::wrap(true, fopen(...), $path, $mode . ($isNonBlocking ? '' : 'n'), (bool) ($options & STREAM_USE_PATH), $this->context);
-        if (!$fp) {
-            return false;
-        }
-
-        // {@see Unblocker::unblock()}
-        $this->id = $id = get_resource_id($fp);
-        self::$resources[$id] = $fp;
-        self::$results[$id] = null; // we don't actually have the result pointer
-
-        // {@see Unblocker::stream_open()}
-        $this->fp = $fp;
-        $this->mode = $mode;
-        $this->options = $options;
-        $this->path = $path;
-
-        $this->pretendNonBlocking = $isNonBlocking;
-
-        // We added 'n' to the mode string, but we'll call stream_set_blocking() as well
-        stream_set_blocking($this->fp, false);
-//        $this->suspend();
+        \rewinddir($this->dh);
         return true;
     }
 
-    public function stream_metadata(string $path, int $option, mixed $value): bool {
-//        $this->suspend();
-        switch ($option) {
-            case STREAM_META_TOUCH:
-                $result = self::wrap(touch(...), $path, $value[0], $value[1]);
-                break;
-            case STREAM_META_OWNER_NAME:
-            case STREAM_META_OWNER:
-                $result = self::wrap(chown(...), $path, $value);
-                break;
-            case STREAM_META_GROUP_NAME:
-            case STREAM_META_GROUP:
-                $result = self::wrap(chgrp(...), $path, $value);
-                break;
-            case STREAM_META_ACCESS:
-                $result = self::wrap(chmod(...), $path, $value);
-                break;
-        }
-        throw new \Exception("stream_metadata not implemented");
+    public function mkdir(string $path, int $mode, int $options): bool {
+        return static::wrap(function() use ($path, $mode, $options) {
+            return \mkdir($path, $mode, 0 !== $options & \STREAM_MKDIR_RECURSIVE, $this->context);
+        });
+    }
+
+    public function rename(string $path_from, string $path_to): bool {
+        return static::wrap(function() use ($path_from, $path_to) {
+            return \rename($path_from, $path_to, $this->context);
+        });
+    }
+
+    public function rmdir(string $path, int $options): bool {
+        return static::wrap(function() use ($path, $options) {
+            return \rmdir($path, $this->context);
+        });
     }
 
     public function unlink(string $path): bool {
-//        $this->suspend();
-        return self::wrap(true, unlink(...), $path);
+        return static::wrap(function() use ($path) {
+            return \unlink($path, $this->context);
+        });
     }
 
-    public function url_stat(string $path, $flags): array|false {
-//        $this->suspend();
+    public function url_stat(string $path, int $flags): array|false {
         if ($flags & STREAM_URL_STAT_LINK) {
-            return self::wrap(true, lstat(...), $path);
+            return static::wrap(function() use ($path, $flags) {
+                if ($flags & \STREAM_URL_STAT_QUIET) {
+                    return @\lstat($path);
+                } else {
+                    return \lstat($path);
+                }
+            });
         } else {
-            return self::wrap(true, stat(...), $path);
+            return static::wrap(function() use ($path, $flags) {
+                if ($flags & \STREAM_URL_STAT_QUIET) {
+                    return @\stat($path);
+                } else {
+                    return \stat($path);
+                }
+            });
         }
     }
+
+    private function readable(): void {
+        if (!Fiber::getCurrent()) {
+            //echo "blocking readable\n";
+            do {
+                $reads = [ $this->fp ];
+                $void = [];
+                $count = \stream_select($reads, $void, $void, 1, 0);
+            } while ($count !== 1);
+        } else {
+            //echo "async readable\n";
+            Co::readable($this->fp);
+        }
+    }
+
+    private function writable(): void {
+        if (!Fiber::getCurrent()) {
+            //echo "blocking writable\n";
+            do {
+                $writes = [ $this->fp ];
+                $void = [];
+                $count = \stream_select($void, $reads, $void, 1, 0);
+            } while ($count !== 1);
+        } else {
+            //echo "async writable\n";
+            Co::writable($this->fp);
+        }
+    }
+
+    private static function suspend(): void {
+        if (Fiber::getCurrent()) {
+            Co::suspend();
+        }
+    }
+
 }
